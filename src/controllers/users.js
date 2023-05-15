69ec2791fed8b8bc7b020c9a19f798f6e2ffba7f
Faruq-Hameed <faruqhameed1@gmail.com>
2023-05-15 18:58:59 +0100

    added controllers

--- src/controllers/users.js
+++ src/controllers/users.js
@@ -1,5 +1,15 @@
 const User = require('../models/user');
+const StatusCodes = require('http-status-codes')
 
 const createUser = async (req, res) => {
-    co
+    const {username, password} = req.body;  //extract data from request body
+     
+    // Check if the username already exists
+    const userAlreadyExist = await User.findOne({username}, 'username')
+    if(userAlreadyExist){
+        return res
+        .status(StatusCodes.CONFLICT)
+        .json({message: `username, ${userAlreadyExist.username} already exists`})
+    }
+
 }
