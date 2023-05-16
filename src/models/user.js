a6c9bd35c218efe3c528c81657a8ea50fb937b11
Faruq-Hameed <faruqhameed1@gmail.com>
2023-05-16 22:22:06 +0100

    added some codes

--- src/models/user.js
+++ src/models/user.js
@@ -1,4 +1,5 @@
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
 const UserSchema = new mongoose.Schema(
   {
     username: {
@@ -11,7 +12,28 @@ const UserSchema = new mongoose.Schema(
   { timestamps: true }
 );
 
-  // User model
-const User = mongoose.model('User', UserSchema);
+// Pre-save middleware to hash the password
+UserSchema.pre("save", function (next) {
+  const user = this;
+  if (!user.isModified("password")) {
+    return next();
+  }
+
+  bcrypt
+    .hash(user.password, 10)
+    .then((hashedPassword) => {
+      user.password = hashedPassword;
+      next();
+    })
+    .catch((err) => next(err));
+});
+// Method to compare passwords
+UserSchema.methods.comparePassword = async function (user) {
+  const isMatch = await bcrypt.compare(user, this.password);
+  return isMatch;
+};
+
+// User model
+const User = mongoose.model("User", UserSchema);
 
 module.exports = User;
