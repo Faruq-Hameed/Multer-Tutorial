7a64750b85d4932847ab484c124fe187c44f3a5f
Faruq-Hameed <faruqhameed1@gmail.com>
2023-05-21 10:21:08 +0100

    tested file uploads. Good!

--- src/controllers/upload.js
+++ src/controllers/upload.js
@@ -1,48 +1,47 @@
-const multer = require('multer');
+const multer = require("multer");
 
 // Set up Multer storage configuration
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
-    cb(null, 'uploads/');
+    cb(null, "uploads/");
   },
   filename: (req, file, cb) => {
     cb(null, file.originalname);
-  }
+  },
 });
 
 // Create a Multer upload instance
 const upload = multer({
   storage: storage,
   limits: {
-    fileSize: 1024 * 1024 * 5 // 5MB file size limit
+    fileSize: 1024 * 1024 * 5, // 5MB file size limit
   },
   fileFilter: (req, file, cb) => {
     // Accept only certain file types
     if (
-      file.mimetype === 'image/jpeg' ||
-      file.mimetype === 'image/png' ||
-      file.mimetype === 'image/gif'
+      file.mimetype === "image/jpeg" ||
+      file.mimetype === "image/png" ||
+      file.mimetype === "image/gif"
     ) {
       cb(null, true);
     } else {
-      cb(new Error('Invalid file type'));// Reject the file
+      cb(new Error("Invalid file type")); // Reject the file
     }
-  }
+  },
 });
 // Define the controller method to handle file upload
 const uploadSingleFile = (req, res) => {
   const file = req.file;
 
   if (!file) {
-    res.status(400).json({ error: 'No file uploaded' });
+    res.status(400).json({ error: "No file uploaded" });
   } else {
-    res.status(200).json({ message: 'File uploaded successfully' });
+    res.status(200).json({ message: "File uploaded successfully" });
   }
 };
 
-
 //error handling middleware
-const uploadError = (err, req, res) => {
+const uploadError = (err, req, res, next) => {
   if (err instanceof multer.MulterError) {
     // Multer errors
     if (err.code === "LIMIT_FILE_SIZE") {
@@ -58,4 +57,4 @@ const uploadError = (err, req, res) => {
   }
 };
 
-module.exports = { uploadSingleFile, upload,uploadError };
+module.exports = { uploadSingleFile, upload, uploadError };
