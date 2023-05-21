0dad2094a0a0c6a0103d754239d14ff68774decb
Faruq-Hameed <faruqhameed1@gmail.com>
2023-05-21 10:00:15 +0100

    worked on file upload controller

--- router.js
+++ router.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const {createUser,getAllUsers,login} = require('./src/controllers/users');
-
+const {uploadSingleFile, upload,uploadError} = require('./src/controllers/upload');
 const router = express.Router()
 
 router.post('/register', createUser)
 router.post('/login', login)
+router.post('/upload', upload.single('file'), uploadSingleFile,uploadError);
 
 router.get('/', getAllUsers)
 
--- src/controllers/upload.js
+++ src/controllers/upload.js
@@ -1,18 +1,61 @@
 const multer = require('multer');
-const path = require('path');
 
+// Set up Multer storage configuration
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
-    cb(null, path.join(__dirname, 'path/to/your/uploads/'));
+    cb(null, 'uploads/');
   },
   filename: (req, file, cb) => {
     cb(null, file.originalname);
   }
 });
 
-const upload = multer({ storage });
-
-// Use the upload middleware in your route handlers
-app.post('/upload', upload.single('file'), (req, res) => {
-  // Handle the file upload
+// Create a Multer upload instance
+const upload = multer({
+  storage: storage,
+  limits: {
+    fileSize: 1024 * 1024 * 5 // 5MB file size limit
+  },
+  fileFilter: (req, file, cb) => {
+    // Accept only certain file types
+    if (
+      file.mimetype === 'image/jpeg' ||
+      file.mimetype === 'image/png' ||
+      file.mimetype === 'image/gif'
+    ) {
+      cb(null, true);
+    } else {
+      cb(new Error('Invalid file type'));// Reject the file
+    }
+  }
 });
+// Define the controller method to handle file upload
+const uploadSingleFile = (req, res) => {
+  const file = req.file;
+
+  if (!file) {
+    res.status(400).json({ error: 'No file uploaded' });
+  } else {
+    res.status(200).json({ message: 'File uploaded successfully' });
+  }
+};
+
+
+//error handling middleware
+const uploadError = (err, req, res) => {
+  if (err instanceof multer.MulterError) {
+    // Multer errors
+    if (err.code === "LIMIT_FILE_SIZE") {
+      res.status(400).json({ error: "File size exceeds the limit" });
+    } else if (err.message === "Invalid file type") {
+      res.status(400).json({ error: "Invalid file type" });
+    } else {
+      res.status(400).json({ error: err.message });
+    }
+  } else {
+    // Other errors
+    res.status(err.status || 500).json({ error: err.message });
+  }
+};
+
+module.exports = { uploadSingleFile, upload,uploadError };
