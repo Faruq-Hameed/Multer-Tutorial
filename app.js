9e2a736daf3b285e74eefa319ad803ba6511e1b1
Faruq-Hameed <faruqhameed1@gmail.com>
2023-05-21 08:41:10 +0100

    added file upload controller

--- app.js
+++ app.js
@@ -17,26 +17,6 @@ app.get('/', (req, res) => {
     res.send('Welcome to the Multer Express MongoDB tutorial!');
   });
   
-  // Set up multer storage and file filter
-  const storage = multer.diskStorage({
-    destination: function (req, file, cb) {
-      cb(null, "uploads/");
-    },
-    filename: function (req, file, cb) {
-      cb(null, file.originalname);
-    },
-  });
-  
-  const upload = multer({ storage: storage });
-  
-  // Use the upload middleware for handling file uploads
-  app.post("/upload", upload.single("file"), (req, res) => {
-    // Handle the uploaded file
-    console.log(req.file);
-    res.send("File uploaded successfully.");
-  });
-
-
 //middleware to handle unknown routes requests  
 app.get('*', (req, res) => {
     res.status(404).send('unknown url')
--- src/controllers/upload.js
+++ src/controllers/upload.js
@@ -1,7 +1,18 @@
-const express = require("express");
+const multer = require('multer');
+const path = require('path');
 
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, path.join(__dirname, 'path/to/your/uploads/'));
+  },
+  filename: (req, file, cb) => {
+    cb(null, file.originalname);
+  }
+});
 
-// Start the server
-app.listen(3000, () => {
-  console.log("Server started on port 3000");
+const upload = multer({ storage });
+
+// Use the upload middleware in your route handlers
+app.post('/upload', upload.single('file'), (req, res) => {
+  // Handle the file upload
 });
